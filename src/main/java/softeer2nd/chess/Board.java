8ce9a9b60a6fba97ee91c71eeb4842fd52c13b56
jangdongho <ehdgh25@naver.com>
2023-07-05 11:34:27 +0900

    feat: Map으로 기물 정보 저장 및 보드 현황 메서드 구현

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -2,70 +2,124 @@ package softeer2nd.chess;
 
 import softeer2nd.chess.exception.InvalidColorException;
 import softeer2nd.chess.pieces.Piece;
+import softeer2nd.chess.pieces.Type;
 
-import java.util.ArrayList;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
 
 public class Board {
 
-    private List<Piece> whitePawns = new ArrayList<>();
-    private List<Piece> blackPawns = new ArrayList<>();
+    private HashMap<Type, List<Piece>> whitePieces = new HashMap<>();
+    private HashMap<Type, List<Piece>> blackPieces = new HashMap<>();
 
-    private final String EMPTY_BOARD = "********";
+    public static final int ROW = 8;
+    public static final int COl = 8;
+
+    public static final String EMPTY_BOARD = "........";
 
     public void end() {
-        whitePawns = null;
-        blackPawns = null;
+        // map의 list에서 dangling pointer 발생안하는지 체크 필요
+        whitePieces = null;
+        blackPieces = null;
     }
 
     public void addWhitePawn(Piece pawn) {
-        verifyWhitePawn(pawn);
-        whitePawns.add(pawn);
+        verifyWhitePiece(pawn);
+        List<Piece> pieces = whitePieces.getOrDefault(Type.PAWN, new ArrayList<>());
+        whitePieces.putIfAbsent(Type.PAWN, pieces);
     }
 
-    private void verifyWhitePawn(Piece pawn) {
-        if(!pawn.getColor().equals(Piece.WHITE_COLOR))
+    private void verifyWhitePiece(Piece piece) {
+        if(!piece.getColor().equals(Piece.WHITE_COLOR))
             throw InvalidColorException.EXCEPTION;
     }
 
     public void addBlackPawn(Piece pawn) {
-        verifyBlackPawn(pawn);
-        blackPawns.add(pawn);
+        verifyBlackPiece(pawn);
+        List<Piece> pieces = blackPieces.getOrDefault(Type.PAWN, new ArrayList<>());
+        blackPieces.putIfAbsent(Type.PAWN, pieces);
     }
 
-    private void verifyBlackPawn(Piece pawn) {
-        if(!pawn.getColor().equals(Piece.BLACK_COLOR))
+    private void verifyBlackPiece(Piece piece) {
+        if(!piece.getColor().equals(Piece.BLACK_COLOR))
             throw InvalidColorException.EXCEPTION;
     }
 
     public int size() {
-        return whitePawns.size();
+        return whitePieces.get(Type.PAWN).size();
     }
 
     public Piece findWhitePawn(int idx) {
-        return whitePawns.get(idx);
+        return whitePieces.get(Type.PAWN).get(idx);
     }
 
     public Piece findBlackPawn(int idx) {
-        return blackPawns.get(idx);
+        return blackPieces.get(Type.PAWN).get(idx);
     }
 
     public void initialize() {
-        whitePawns = new ArrayList<>();
-        blackPawns = new ArrayList<>();
 
-        for (int i = 0; i < 8; i++) {
-            whitePawns.add(Piece.createWhitePawn());
-            blackPawns.add(Piece.createBlackPawn());
+        // 초기화 및 출력시에는 배열 사용 고려
+        List<String> initBoard = new ArrayList<>();
+        for (int i = 0; i < COl; i++) {
+            initBoard.add(StringUtils.appendNewLine(EMPTY_BOARD));
         }
+
+        for (Type type : Type.values()) {
+            whitePieces.put(type, new ArrayList<>());
+            blackPieces.put(type, new ArrayList<>());
+        }
+
+        for (int i = 0; i < COl; i++) {
+
+            List<Piece> blackPawns = blackPieces.get(Type.PAWN);
+            List<Piece> whitePawns = whitePieces.get(Type.PAWN);
+            blackPawns.add(Piece.createBlackPawn(new Point(i, 1)));
+            whitePawns.add(Piece.createWhitePawn(new Point(i, 6)));
+        }
+
+        for (int i = 0; i < COl; i += 7) {
+            List<Piece> blackRooks = blackPieces.get(Type.ROOK);
+            List<Piece> whiteRooks = whitePieces.get(Type.ROOK);
+            blackRooks.add(Piece.createBlackRook(new Point(i, 0)));
+            whiteRooks.add(Piece.createWhiteRook(new Point(i, 7)));
+        }
+
+        for (int i = 1; i < COl; i += 5) {
+            List<Piece> blackKnights = blackPieces.get(Type.KNIGHT);
+            List<Piece> whiteKnights = whitePieces.get(Type.KNIGHT);
+            blackKnights.add(Piece.createBlackKnight(new Point(i, 0)));
+            whiteKnights.add(Piece.createWhiteKnight(new Point(i, 7)));
+
+        }
+
+        for (int i = 2; i < COl; i += 3) {
+            List<Piece> blackBishops = blackPieces.get(Type.BISHOP);
+            List<Piece> whiteBishops = whitePieces.get(Type.BISHOP);
+            blackBishops.add(Piece.createBlackBishop(new Point(i, 0)));
+            whiteBishops.add(Piece.createWhiteBishop(new Point(i, 7)));
+
+        }
+
+        List<Piece> blackQueens = blackPieces.get(Type.QUEEN);
+        List<Piece> whiteQueens = whitePieces.get(Type.QUEEN);
+        blackQueens.add(Piece.createBlackQueen(new Point(3, 0)));
+        whiteQueens.add(Piece.createWhiteQueen(new Point(3, 7)));
+
+        List<Piece> blackKings = blackPieces.get(Type.KING);
+        List<Piece> whiteKings = whitePieces.get(Type.KING);
+        blackKings.add(Piece.createBlackKing(new Point(4, 0)));
+        whiteKings.add(Piece.createWhiteKing(new Point(4, 7)));
+
     }
 
     public String getWhitePawnsResult() {
-        return getPawnsResult(whitePawns);
+        return getPawnsResult(whitePieces.get(Type.PAWN));
     }
 
     public String getBlackPawnsResult() {
-        return getPawnsResult(blackPawns);
+        return getPawnsResult(blackPieces.get(Type.PAWN));
     }
 
     private String getPawnsResult(List<Piece> whitePawns) {
@@ -79,18 +133,52 @@ public class Board {
     }
 
     public void print() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 8; i++) {
+        System.out.println(showBoard());
+    }
 
-            if(i == 1) {
-                sb.append(StringUtils.appendNewLine(getBlackPawnsResult()));
-            } else if (i == 6) {
-                sb.append(StringUtils.appendNewLine(getWhitePawnsResult()));
-            } else {
-                sb.append(StringUtils.appendNewLine(EMPTY_BOARD));
+    public int pieceCount() {
+        int whitePieceCount = 0;
+        for (Type type : Type.values()) {
+            whitePieceCount += whitePieces.get(type).size();
+        }
+
+        int blackPieceCount = 0;
+        for (Type type : Type.values()) {
+            blackPieceCount += blackPieces.get(type).size();
+        }
+
+        return whitePieceCount + blackPieceCount;
+    }
+
+    public String showBoard() {
+        char[][] board = new char[ROW][COl];
+        for (int i = 0; i < board.length; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        for (Type type : Type.values()) {
+            List<Piece> whitePieces = this.whitePieces.get(type);
+            List<Piece> blackPieces = this.blackPieces.get(type);
+
+            for (int i = 0; i < whitePieces.size(); i++) {
+                Piece piece = whitePieces.get(i);
+                Point point = piece.getPoint();
+                board[point.y][point.x] = piece.getRepresentation();
+            }
+
+            for (int i = 0; i < blackPieces.size(); i++) {
+                Piece piece = blackPieces.get(i);
+                Point point = piece.getPoint();
+                board[point.y][point.x] = piece.getRepresentation();
             }
         }
 
-        System.out.println(sb);
+        StringBuilder boardStatus = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            boardStatus.append(StringUtils.appendNewLine(String.valueOf(board[i])));
+        }
+
+        return boardStatus.toString();
     }
 }
+
