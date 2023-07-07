18a0aa15236a6c5d45eb2604e9cc9517309192d3
jangdongho <ehdgh25@naver.com>
2023-07-07 10:17:23 +0900

    test: 퀸 이동 관련 TC 작성

--- src/test/java/softeer2nd/chess/ChessGameTest.java
+++ src/test/java/softeer2nd/chess/ChessGameTest.java
@@ -7,12 +7,12 @@ import org.junit.jupiter.api.Test;
 import softeer2nd.chess.exception.InvalidColorException;
 import softeer2nd.chess.exception.SameTeamExistException;
 import softeer2nd.chess.pieces.Color;
+import softeer2nd.chess.pieces.exception.InvalidMoveException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static softeer2nd.chess.pieces.Piece.*;
 
 class ChessGameTest {
-
     private Board board;
 
     @BeforeEach
@@ -111,5 +111,104 @@ class ChessGameTest {
         Assertions.assertThrows(
                 SameTeamExistException.class,
                 () -> chessGame.move(sourcePosition, targetPosition));
+    }
+
+    @Test
+    @DisplayName("퀸 직선 이동 성공")
+    public void moveStraightQueenSuccess() {
+        // given
+        board.initializeEmpty();
+        ChessGame chessGame = new ChessGame(board);
+
+        String sourcePosition = "e1";
+        String targetPosition = "e4";
+
+        board.addBlackPiece(createBlackQueen(sourcePosition));
+
+        // when
+        chessGame.move(sourcePosition, targetPosition);
+
+        // then
+        assertEquals(createBlackQueen(new Position(targetPosition)), chessGame.findPiece(targetPosition));
+    }
+
+    @Test
+    @DisplayName("퀸 대각선 이동 성공")
+    public void moveDiagonalQueenSuccess() {
+        // given
+        board.initializeEmpty();
+        ChessGame chessGame = new ChessGame(board);
+
+        String sourcePosition = "e1";
+        String targetPosition = "a5";
+
+        board.addBlackPiece(createBlackQueen(sourcePosition));
+
+        // when
+        chessGame.move(sourcePosition, targetPosition);
+
+        // then
+        assertEquals(createBlackQueen(new Position(targetPosition)), chessGame.findPiece(targetPosition));
+    }
+
+    @Test
+    @DisplayName("퀸 불가능한 위치로 이동")
+    public void moveQueenFailure() {
+        // given
+        board.initializeEmpty();
+        ChessGame chessGame = new ChessGame(board);
+
+        String sourcePosition = "e1";
+        String targetPosition = "a7";
+
+        board.addBlackPiece(createBlackQueen(sourcePosition));
+
+        // when then
+        assertThrows(
+                InvalidMoveException.class,
+                () -> chessGame.move(sourcePosition, targetPosition)
+        );
+    }
+
+    @Test
+    @DisplayName("퀸 이동 경로에 기물 위치")
+    public void existPieceOnQueenMoveFailure() {
+        // given
+        board.initializeEmpty();
+        ChessGame chessGame = new ChessGame(board);
+
+        String sourcePosition = "e1";
+        String targetPosition = "h4";
+        String blockPosition = "g3";
+
+        board.addBlackPiece(createBlackQueen(sourcePosition));
+        board.addWhitePiece(createWhiteBishop(blockPosition));
+
+        // when then
+        assertThrows(
+                InvalidMoveException.class,
+                () -> chessGame.move(sourcePosition, targetPosition)
+        );
+    }
+
+    @Test
+    @DisplayName("퀸 도착 위치에 같은팀 기물 위치")
+    public void existSameTeamPieceOnTargetFailure() {
+        // given
+        board.initializeEmpty();
+        ChessGame chessGame = new ChessGame(board);
+
+        String sourcePosition = "e1";
+        String targetPosition = "h4";
+        String teamPiecePosition = "h4";
+
+        board.addBlackPiece(createBlackQueen(sourcePosition));
+        board.addBlackPiece(createBlackBishop(teamPiecePosition));
+
+        // when then
+        assertThrows(
+                SameTeamExistException.class,
+                () -> chessGame.move(sourcePosition, targetPosition)
+        );
     }
 }
